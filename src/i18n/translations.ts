94964ce7bd4eccc0276276725a5df3cbb8b9d0d5
alex <reyz999@gmail.com>
2025-12-21 19:37:52 -0700

    language test, new translations.ts

---
+++ src/i18n/translations.ts
@@ -0,0 +1,13 @@
+import en from "../locales/en/common.json";
+import es from "../locales/es/common.json";
+
+export const DEFAULT_LANGUAGE = "en" as const;
+
+export const supportedLanguages = ["en", "es"] as const;
+
+export type Language = typeof supportedLanguages[number];
+
+export const translations: Record<Language, Record<string, string>> = {
+    en,
+    es,
+};
